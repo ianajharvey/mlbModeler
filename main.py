3fe93c30465085b36258501467d32080bc6a1c95
ianajharvey <ianajharvey@gmail.com>
2025-06-05 22:46:30 -0400

    Initial Commit

---
+++ main.py
@@ -0,0 +1,8 @@
+import statsapi as sts
+
+schedule = sts.schedule(start_date="06/01/2025",
+                        end_date="06/05/2025",
+                        team="",
+                        opponent="")
+
+print(schedule)
