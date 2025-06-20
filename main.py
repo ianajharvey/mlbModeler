3ab444d6d19a2e924f484ebca9d449d7f7800b93
ianajharvey <ianajharvey@gmail.com>
2025-06-19 21:57:13 -0400

    Create a DF of game scores for a particular team

--- main.py
+++ main.py
@@ -1,8 +1,20 @@
-import statsapi as sts
+import statsapi
+import pandas as pd
 
-schedule = sts.schedule(start_date="06/01/2025",
-                        end_date="06/05/2025",
-                        team="",
+schedule = statsapi.schedule(start_date="03/27/2025",
+                        end_date="06/18/2025",
+                        team="111",
                         opponent="")
 
-print(schedule)
+
+gameScores = []
+
+for game in schedule:
+    gamescore = {"Home Team": game["home_name"],"Home ID": game["home_id"], "Home Score": game["home_score"],
+                 "Away Team": game["away_name"],"Away ID": game["away_id"], "Away Score": game["away_score"]}
+    gameScores.append(gamescore)
+
+df = pd.DataFrame(gameScores)
+
+print(df)
+#print(statsapi.get('team', {'teamId':143}))
