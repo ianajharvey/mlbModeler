0813d923197e1cedb86d6f2d19b0799b0177fa21
ianajharvey <ianajharvey@gmail.com>
2025-06-28 22:53:35 -0400

    generate single line team advanced stats for passing to team data

--- rosterScorer.py
+++ rosterScorer.py
@@ -1,4 +1,5 @@
 import statsapi
+import pandas as pd
 
 def scoreoffensiveRoster(positionRoster):
     score_dict_list = []
@@ -22,7 +23,31 @@ def scoreoffensiveRoster(positionRoster):
 
         score_dict_list.append(playerDict)
 
-    return score_dict_list
+    off_df = pd.DataFrame(score_dict_list)
+
+    team_totals = off_df.sum(numeric_only=True)
+    team_stat_dict = team_totals.to_dict()
+
+    team_average = team_stat_dict["Career_hits"] / team_stat_dict["Career_atBats"]
+    team_obp = (team_stat_dict["Career_hits"] + team_stat_dict["Career_walks"] + team_stat_dict["Career_hbp"]) / team_stat_dict["Career_plateAppearances"]
+    team_slugging = team_stat_dict["Career_totalBases"] / team_stat_dict["Career_atBats"]
+    team_ops = team_obp + team_slugging
+    team_EtotalBases = (team_stat_dict["Career_totalBases"] + team_stat_dict["Career_walks"] + team_stat_dict["Career_hbp"] + team_stat_dict["Career_stolenBases"] - team_stat_dict["Career_caughtStealing"])
+    team_Eslugging = team_EtotalBases / team_stat_dict["Career_plateAppearances"]
+    team_EOPS = team_Eslugging + team_obp
+    team_rbipa = team_stat_dict["Career_rbi"] / team_stat_dict["Career_plateAppearances"]
+    team_walkRate = team_stat_dict["Career_walks"] / team_stat_dict["Career_plateAppearances"]
+    team_iso = team_slugging - team_average
+    team_sbEfficiency = team_stat_dict["Career_stolenBases"] / (team_stat_dict["Career_stolenBases"] + team_stat_dict["Career_caughtStealing"])
+    team_Eadvancement = team_EtotalBases / team_stat_dict["Career_plateAppearances"]
+
+    off_dict = {"average": team_average, "obp": team_obp, "slugging": team_slugging, "ops": team_ops,
+                "e_totalBases": team_EtotalBases, "e_slugging": team_Eslugging, "e_ops": team_EOPS,
+                "rbipa": team_rbipa, "walkRate": team_walkRate, "iso": team_iso, "sb_efficiency":team_sbEfficiency,
+                "E+advancement": team_Eadvancement}
+
+
+    return off_dict
 
 
 def scorePitchingRoster(pitchingRoster):
